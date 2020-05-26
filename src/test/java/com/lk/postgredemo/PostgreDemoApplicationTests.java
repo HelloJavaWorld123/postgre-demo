2fec42245f39b873f9e003360c08b543a1c4eec2
xiaokuan.ren <renxiaokuan9428@duia.com>
2020-05-26 12:34:49 +0800

    init

---
+++ src/test/java/com/lk/postgredemo/PostgreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lk.postgredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
